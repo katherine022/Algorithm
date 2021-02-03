74fab15675f2927386b10e6157c705f2bfcd1c03
nylee <lny7036@gmail.com>
2021-02-03 11:48:56 +0900

    1904번. 01타일

---
+++ 1904번. 01타일/1904번. 01타일/1904.cpp
@@ -0,0 +1,30 @@
+//https://www.acmicpc.net/problem/1904
+//01≈∏¿œ
+//2020,02.03
+//C++
+//skyna
+
+#include<iostream>
+#include<algorithm>
+
+const int MAX = 1000001;
+using namespace std;
+
+int dp[MAX];
+
+int fibo(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	if (dp[n] != 0) return dp[n];
+
+	dp[n] = (fibo(n - 1) + fibo(n - 2)) % 15746;
+	return dp[n];
+}
+
+int main() {
+	int n;
+	cin >> n;
+	fill(dp, dp + MAX, 0);
+	dp[0] = 1;
+	cout << fibo(n);
+}
